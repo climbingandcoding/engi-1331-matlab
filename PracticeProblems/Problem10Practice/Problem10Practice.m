fc3f5c3889186367144b685fb792eb7789f1cdbd
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-07-26 04:52:48 -0500

    Update Problem10Practice.m

--- PracticeProblems/Problem10Practice/Problem10Practice.m
+++ PracticeProblems/Problem10Practice/Problem10Practice.m
@@ -1,5 +1,5 @@
 function [volume] = Problem10Practice(radius, height)
-% Problem 10 Practice Solution. This problem covers functions, and the dot 
+% Problem 10 Practice Solution. This problem covers functions and the dot 
 % operator.
 
 % Finds the volume of the cylinder.
