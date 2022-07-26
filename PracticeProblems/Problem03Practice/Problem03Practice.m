a107e2d8df3e3b760b7dc29a81aa7ea784a6a3fa
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-07-26 04:46:43 -0500

    Update Problem03Practice.m

--- PracticeProblems/Problem03Practice/Problem03Practice.m
+++ PracticeProblems/Problem03Practice/Problem03Practice.m
@@ -1,8 +1,8 @@
 function [radiusM, heightM, material] = Problem3Practice()
 % Problem 3 Practice Solution. This problem covers inputs and functions.
 
-% Handles the input from the user directly to the function. We will almost
-% certainly never use this is in ENGI 1331.
+% Handles the input from the user directly to the function. We will likely
+% never use this is in ENGI 1331.
 import IO_Handler.*;
 
 % Takes the input of the radius directly from the user.
@@ -12,7 +12,7 @@ radiusM = input('Enter a radius [m]: ');
 heightM = input('Enter a height [m]: ');
 
 % Takes the input of the material directly from the user. The 's' is needed
-% because inputs only accept numerical entries unless the 's' string
+% because the input command only accepts numerical entries unless the 's' string
 % identifier is present.
 material = input('Enter a cylinder material: ', 's');
 
