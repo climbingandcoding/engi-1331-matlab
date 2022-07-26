a14d2d777a2394c5ae72274616452a87a01f6542
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-07-26 04:40:27 -0500

    Add files via upload

---
+++ PracticeProblems/Problem04Practice/Problem04Practice.m
@@ -0,0 +1,15 @@
+function [surfaceTension, maximumRadiusM, liquidDensity] = Problem4Practice(maximumRadiusIN, specificGravity, densityWater, gravity)
+% Problem 4 Practice Solution. This problem covers functions.
+
+% Finds the maximum radius of the droplet in meters. We do not need the dot
+% operator since we are multiplying by a scalar.
+maximumRadiusM = maximumRadiusIN * 0.0254;
+
+% Finds the density of the liquid.
+liquidDensity = specificGravity * densityWater;
+
+% Finds the surface tension of the droplet.
+surfaceTension = maximumRadiusM^2 * gravity * liquidDensity;
+
+
+end
