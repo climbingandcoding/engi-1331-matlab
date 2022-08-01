866a03cb657eb7e14e71f7b43d064be1985ea369
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-08-01 05:02:26 -0500

    Add files via upload

---
+++ PracticeProblems/Problem26Practice/Problem26Practice.m
@@ -0,0 +1,27 @@
+function [uniqueIndicator] = Problem26Practice(vectorInput)
+% Problem 25 Practice Solution. This problem covers 'for' loops, 'if' 
+% statements, 'length', the 'not' (~) operator, and functions.
+
+% This line assumes that the vector is unique. In the next lines, we will
+% test this hypothesis and update it if necessary.
+uniqueIndicator = 1;
+
+% This line creates a vector from 1 to the length of the 'vectorInput'. If
+% the input vector is unique, it will be identical to this vector.
+comparisonVector = 1:length(vectorInput);
+
+% Creates the 'for' loop that will go through the entire input vector.
+for i = 1:length(vectorInput)
+
+    % This line checks whether 'comparisonVector' is equal to the
+    % 'vectorInput' up to the current index 'i'.
+    if sum(comparisonVector(i) == vectorInput) ~= 1
+        
+        uniqueIndicator = 0;
+        
+    end
+    
+end
+
+
+end
