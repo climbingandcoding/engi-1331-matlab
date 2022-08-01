51d3f9905fdc8dc5067979ada26068d4e6000b5b
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-08-01 06:17:21 -0500

    Update Problem29Practice.m

--- PracticeProblems/Problem29Practice/Problem29Practice.m
+++ PracticeProblems/Problem29Practice/Problem29Practice.m
@@ -2,18 +2,18 @@
 
 % This problem covers anonymous functions, 'fzero', and 'integral'.
 
-% These lines define y1 and y2 as function handles (anonymous functions).
+% These lines define 'y1' and 'y2' as function handles (anonymous functions).
 y1 = @(x) 3.2 * x - 9;
 y2 = @(x) 0.6 * x + 2;
 
-% These lines calculate the zeros of y1 and y2.
+% These lines calculate the zeros of 'y1' and 'y2'.
 xZ1 = fzero(y1, 5);
 xZ2 = fzero(y2, 5);
 
 % This line defines the intersection function.
 inter = @(x) y1(x) - y2(x);
 
-%These lines find the intersection point.
+% These lines find the intersection point.
 Xint = fzero(inter, 5);
 Yint = y1(Xint);
 
