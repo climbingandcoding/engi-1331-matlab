1f8b8014388f95e23f5ea9237d8360bf71880389
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-08-14 14:40:32 -0500

    Update Problem37Practice.m

--- PracticeProblems/Problem37Practice/Problem37Practice.m
+++ PracticeProblems/Problem37Practice/Problem37Practice.m
@@ -8,8 +8,7 @@ TL = [375, 395, 420, 450, 500];
 VL = [2.2, 2.3, 2.5, 2.6, 2.9];
 
 % This line uses 'polyfit' to find the coefficients of the best-fitting
-% linear curve. The '1' is because a linear curve is a polynomial of degree
-% '1'.
+% linear curve.
 CoeffsL = polyfit(TL, VL, 1);
 
 % These lines defines the 'm' and 'b' values.
