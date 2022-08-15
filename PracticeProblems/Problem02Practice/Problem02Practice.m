e905a4aef3a9507864dfda02f0a3b49b66631ba3
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-08-15 13:26:55 -0500

    Update Problem02Practice.m

--- PracticeProblems/Problem02Practice/Problem02Practice.m
+++ PracticeProblems/Problem02Practice/Problem02Practice.m
@@ -1,6 +1,5 @@
 function [volumeCM] = Problem02Practice(radiusCM, heightCM)
-% Problem 2 Practice Solution. This problem covers functions and the dot
-% operator.
+% Problem 2 Practice Solution. This problem covers the dot operator and functions.
 
 % Finds the volume in centimeters cubed using the formula v = pi * radius^2
 % * height. The dot operator is used to account for a vector of radius and 
