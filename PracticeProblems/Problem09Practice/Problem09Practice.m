16a960d53eedf629dbbc87c97aa9cb485f792aee
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-08-15 13:32:22 -0500

    Update Problem09Practice.m

--- PracticeProblems/Problem09Practice/Problem09Practice.m
+++ PracticeProblems/Problem09Practice/Problem09Practice.m
@@ -1,6 +1,6 @@
 function [surfaceArea] = Problem09Practice(radius, height)
-% Problem 9 Practice Solution. This problem covers functions and the dot
-% operator.
+% Problem 9 Practice Solution. This problem covers the dot operator
+% and functions.
 
 % Finds the surface area of the cylinder.
 surfaceArea = (2 * pi * radius .* height) + (2 * pi * radius.^2);
