aade1f5af64a74ab5b192e1e3e0b4d797a57d69a
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-08-16 15:43:08 -0500

    Add files via upload

---
+++ Exam2Material/Fall2021AddtionalProblems/Moving_Window_StartingFile(1).m
@@ -0,0 +1,45 @@
+% Name:
+% Date:
+% Moving Window Practice
+
+clc; clear; close all;
+
+% User input:
+% A random vector will be useid in this ICP. The length of this vector is a
+% user input. Prompt the user to enter the length of the vector to be created.
+
+% Data validation:
+% You want the length to be positive. If it is not do the following:
+% If the length is less than zero, take the absolute value of it, and give
+% a warning. If it is zero, give an error and terminate the program.
+
+% Vector creation:
+% Create a row vector of random integers with values from 0 to 100
+% and store it in a variable named X. This row vector should have a length
+% entered by the user.
+
+
+% Create a function with function header: [Results] = function(V)
+% Input:
+%   V (numerical vector)
+% Output:
+%   Results (numerical matrix)
+
+% Purpose: Write a function named MovingWindow that, given a vector, finds the
+% values in that vector, that has a moving average above the current value.
+% What does a moving average mean? Moving average for an element is equal
+% to the average of three values:
+% current value, the previous value and the next value.
+% If the moving average is above the current value, then save the current
+% value, its moving average and location as a row vector. If you have multiple cases,
+% save the value, average and location for each such case as a new row. 
+
+
+
+
+    
+        
+
+
+
+
