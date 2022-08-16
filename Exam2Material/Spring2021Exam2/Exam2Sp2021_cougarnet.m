f40eb60f88505fc4593ed0a3a5c1f6a646d51a2e
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2022-08-16 16:03:22 -0500

    Add files via upload

---
+++ Exam2Material/Spring2021Exam2/Exam2Sp2021_cougarnet.m
@@ -0,0 +1,39 @@
+% Exam 2 - ENGI 1331 
+% Name: 
+% UHID: 
+% Cougarnet Username:
+% Date: April 3, 2021
+
+% NO ADDITIONAL COMMENTS REQUIRED IN CODE 
+% Problem statements, variables define, etc. not required.
+% REMINDER: 
+% Attempt every task.  If you cannot achieve a previous task, 
+% assume values (if necessary) and attempt the next task.  
+% Partial credit will be given throughout. 
+
+% *** IMPORTANT - you must replace insert FULL name and UH ID below for
+% your exam to be grade. 
+
+% I, <INSERT FULL NAME> (<INSERT UHID>) acknowledge that the Exam 2 for ENGI 1331, 
+% is to be completed by myself with no collaboration with anyone.  
+% I have read the ENGI 1331 Position on Academic Honesty and agree 
+% to abide by its provisions while taking this exam. 
+% I acknowledge that my submission will be run through a similarity code. 
+% Any student with unacceptable levels of commonality with peers or 
+% other sources will be brought up for an academic honesty violation.') 
+clc; clear all; close all;
+
+% Task 1
+
+
+% Task 2
+
+
+
+% Task 3
+
+
+
+
+
+
