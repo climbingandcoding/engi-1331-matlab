c4070e886e247179aa0334af802e1cd82e98acf6
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2023-01-28 22:04:46 -0600

    Add files via upload

---
+++ WorkshopContent/Week02/Class04/NewtonGravitationScriptWeek2Class4.m
@@ -0,0 +1,44 @@
+%% Description:
+
+%{
+
+    Week 2 - Class 4 - 08/31/22
+
+    This script is used to call the 'NewtonGravitationCalc' user-defined
+    function. In this file, we will take the user's inputs for 'm1', 'm2',
+    and 'r' using the 'input' function and return the output for 'F' to 
+    the user using the 'fprintf' function.
+
+%}
+
+%% Housekeeping Commands:
+
+% Clears the command window.
+clc
+
+% Clears the workspace of all variables.
+clear
+
+% Closes all figures (plots).
+close all
+
+%% Taking the User's Inputs:
+
+% Take the input for 'm1' here.
+
+
+% Take the input for 'm2' here.
+
+
+% Take the input for 'r' here.
+
+
+%% Calling the 'NewtonGravitationCalc' User-Defined Function:
+
+% Call the 'NewtonGravitationCalc' user-defined function here.
+
+
+%% Printing the Output:
+
+% Print the output to the screen here.
+
