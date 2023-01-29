3017b736a54701adc232a74fc8932d355248ddb9
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2023-01-28 23:45:08 -0600

    Add files via upload

---
+++ WorkshopContent/Week15/Class29/FinalExam_cougarnet.m
@@ -0,0 +1,45 @@
+% Final Exam - ENGI 1331 
+% Name: 
+% UHID: 
+% Cougarnet Username:
+% Date: August 5, 2020
+
+% NO ADDITIONAL COMMENTS REQUIRED IN CODE 
+% Problem statements, variables define, etc. not required.
+% REMINDER: 
+% Attempt every task.  If you cannot achieve a previous task, 
+% assume values (if necessary) and attempt the next task.  
+% Partial credit will be given throughout. 
+
+% *** IMPORTANT - you must replace insert FULL name and UH ID below for
+% your exam to be grade. 
+
+% I, <INSERT FULL NAME> (<INSERT UHID>) acknowledge that  the Final Exam for ENGI 1331, 
+% is to be completed by myself with no collaboration with anyone.  
+% I have read the ENGI 1331 Position on Academic Honesty and agree 
+% to abide by its provisions while taking this exam. 
+% I acknowledge that my submission will be run through a similarity code. 
+% Any student with unacceptable levels of commonality with peers or 
+% other sources will be brought up for an academic honesty violation.') 
+%% Problem 1
+clc; clear all; close all;
+
+
+% Task 1
+
+
+
+% Task 2
+
+
+
+% Task 3
+
+
+
+% Task 4
+
+
+
+% Task 5
+
