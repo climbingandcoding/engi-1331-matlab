8c758e8b62d3b6eed68d6599739e9915a2a65ce4
climbingandcoding <108996474+climbingandcoding@users.noreply.github.com>
2023-01-28 22:17:57 -0600

    Add files via upload

---
+++ WorkshopContent/Week06/Class12/logicals.m
@@ -0,0 +1,50 @@
+% conditionals and logicals
+% piece_wise
+
+% Create a matrix of random values
+
+
+%       y(x) = x^2-x+9         x<=10
+%       900x^3 + 42x -9   10 < x <= 35
+%       72                35 < x <= 100
+
+% Preallocate results matrix by setting it equal to independent variable
+% matrix
+
+
+% Create the first logical array with the first condition from the piecewise
+% function
+
+
+% Apply respective equation to matrix using logical array to obtain results
+
+
+% Create the second logical array with the first condition from the piecewise
+% function
+
+
+% Apply respective equation to matrix using logical array to obtain results
+
+
+% Create the third logical array with the first condition from the piecewise
+% function
+
+
+% Apply respective equation to matrix using logical array to obtain results
+
+%%
+
+
+% Ask the user to enter a vector of the side lengths of their desired shape
+%   (i.e. if the shape is a triangle, 3 side lengths should be entered)
+
+% Using conditional statements, be able to print to the command window the
+%   shape the user entered (i.e. if the shape is a triangle, print "Shape
+%   is a triangle" to the command window)
+
+% Be able to determine if the shape is a TRIANGLE, SQUARE, RECTANGLE,
+%   PENTAGON, or HEXAGON
+web('https://www.amentsoc.org/insects/glossary/terms/dichotomous-key/')
+
+
+
